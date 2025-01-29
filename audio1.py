d33f0e378b289dc2095cbd86d6f23b97bd27787a
saikrishna2553 <saikrishnakannam2553@gmail.com>
2025-01-29 18:53:26 +0530

    Add files via upload

---
+++ audio1.py
@@ -0,0 +1,11 @@
+from gtts import cd
+import os
+text="kk.txt"
+audio="audio.mp3"
+def text_to_audio(text,audio):
+    file=open(text,'r',encoding='utf_8')
+    read_data=file.read()
+    ga=cd(text, lang='te')
+    ga.save(audio)
+    file.close()
+text_to_audio(text,audio)
